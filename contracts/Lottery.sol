917f51c5d9e84c272bd483f03730c3cb92d9dae0
PhilipAtEmcogma <emcogma@gmail.com>
2023-05-23 22:21:18 +0800

    start working on lottery contract via lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,9 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainLink.sol";
+
 //creating the lottery contract
 contract Lottery {
-    constructor() public {
-        
+    using SafeMathChainLink for uint256;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    uint256 usdEntryFee;
+
+    constructor(address _ethUsdPriceFeed) public {
+        ethUsdePriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
+        usdEntryFee = 50;//$50
+    }
+
+    function enter() public payable{
+        require(msg.value >= getEntranceFee(),"Not enough ETH to enter");
+
+    }
+
+    function getEntranceFee() public view returns(uint256){
+        uint256 precision = 1 * 10 ** 18;
+        uint256 price = getLatestEthUsdPrice();
+        uint256 costToEnter = (precision / price) * (usdEntryFee * 100000000);
+    }
+
+    function getLatestEthUsdPrice() public view returns(uint256){
+        {
+            uint80 roundID,
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        } = ethUsdPriceFeed.lastestRoundData();
+        return uint256(price);
+    } 
+
+
+    function startLotter() public {
+
+    }
+
+    function endLottery() public{
+
+    }
+
+    function pickWinner(){
+
     }
 }
