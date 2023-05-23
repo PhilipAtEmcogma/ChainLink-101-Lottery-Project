d8ec30271c442c2eae90675fb042c5955853f65f
PhilipAtEmcogma <emcogma@gmail.com>
2023-05-23 22:47:59 +0800

    add Lottery state to Lottery contract

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,17 +7,22 @@ import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainLink.sol";
 //creating the lottery contract
 contract Lottery {
     using SafeMathChainLink for uint256;
+    enum LOTTERY_STATE {OPEN, CLOSED, CALCULATING_WINNDER};
+    LOTTERY_STATE public lotteryState;
     AggregatorV3Interface internal ethUsdPriceFeed;
     uint256 usdEntryFee;
+    address payable[] public players;
 
     constructor(address _ethUsdPriceFeed) public {
         ethUsdePriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
         usdEntryFee = 50;//$50
+        lotteryState = LOTTER_STATE.CLOSED;
     }
 
     function enter() public payable{
         require(msg.value >= getEntranceFee(),"Not enough ETH to enter");
-
+        require(lotteryState == LOTTERY_STATE.OPEN);//to make sure previous game is finished
+        players.push(msg.sender); //put new round of players into array for new game
     }
 
     function getEntranceFee() public view returns(uint256){
