49164d8d3d0d9569d573e58bf7fa76770d56b3cf
PhilipAtEmcogma <emcogma@gmail.com>
2023-05-24 23:23:17 +0800

    implement pickwinner and endlottery function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,22 +4,31 @@ pragma solidity ^0.8.7.0;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3interface.sol";
 import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainLink.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/0.6/VRFConsumerBase.sol";
 
 //creating the lottery contract
-contract Lottery is Ownable{
+contract Lottery is VRFConsumerBase, Ownable{
     using SafeMathChainLink for uint256;
     enum LOTTERY_STATE {OPEN, CLOSED, CALCULATING_WINNDER};
     LOTTERY_STATE public lotteryState;
     AggregatorV3Interface internal ethUsdPriceFeed;
     uint256 public usdEntryFee;
-    uint256 public randomness;
     address payable[] public players;
+    uint256 public randomness;
+    uint256 public fee;
+    bytes32 public keyHash;
 
-    constructor(address _ethUsdPriceFeed) public {
-
+    //_link = link address
+    constructor(address _ethUsdPriceFeed, address _vrfCoordinator, address _link, bytes32 _keyHash)
+        VRFConsumerBase(
+            _vrfCoordinator,
+            _link
+         ) public{
         ethUsdePriceFeed = AggregatorV3Interface(_ethUsdPriceFeed);
         usdEntryFee = 50;//$50
         lotteryState = LOTTER_STATE.CLOSED;
+        fee = 100000000000000000; //0.1 link, 17s 0
+        keyHash = _keyHash;
     }
 
     function enter() public payable{
@@ -53,11 +62,20 @@ contract Lottery is Ownable{
         randomness = 0;
     }
 
-    function endLottery() public{
-
+    //use chainlink inbuilt random number gen to pick winner
+    function endLottery(uint256 userProvideSeed) public onlyOwner{
+        require(lotteryState == LOTTERY_STATE.OPEN, "Can't end lottery yet");
+        lotteryState = LOTTERY_STATE.CALCULATING_WINNER;
+        pickWinner(userProvideSeed);
     }
 
-    function pickWinner(){
+    function pickWinner(uint256 userProvideSeed) private returns(bytes32){
+        require(lotteryState == LOTTERY_STATE.CALCULATING_WINNER, "Needs to be calculating the winner");
+        bytes32 requestId = requestRandomness(KeyHash, fee, userProvidedSeed);
+    }
 
+    //request randomness
+    function fulfilRandomness(){
+        
     }
 }
